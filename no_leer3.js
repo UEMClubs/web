5b0dab5e3670987d11c420a12ac2a5552b785776
UEMClubs <101675357+UEMClubs@users.noreply.github.com>
2022-06-03 19:10:54 +0200

    Add files via upload

---
+++ no_leer3.js
@@ -0,0 +1,4 @@
+
+document.querySelector('.login_img_btn').addEventListener('click', function() {
+    document.querySelector('.login_cont').classList.toggle('s--signup');
+  });
---
+++ registro.php
@@ -0,0 +1,40 @@
+<?php
+    
+    $dbhost="18.184.6.232";
+    $dbuser="admin";
+    $dbpass="root";
+    $dbname="paginaweb";
+
+    $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+    if(!$conn)
+    {
+        die("No hay conexion:" .mysqli_connect_error());
+    }
+
+
+    $nombre=$_POST["usuario"];
+    $pass=$_POST["pass"];
+    $email=$_POST["email"];
+    
+    //LOGIN
+    if(isset($_POST["btningresar"])){
+
+        $query = mysqli_query($conn,"SELECT * FROM usuarios WHERE usuario ='$nombre' AND password = '$pass'");
+        $nr = mysqli_num_rows($query);
+        if($nr==1){
+            echo "<script>window.location='PRUEBA.html'</script>";
+        }else{
+            echo "<script>alert('Usuario no valido');window.location='login.html'</script>";
+        }
+    }
+
+    //REGISTRAR
+    if(isset($_POST["btnregistrar"])){
+       $sqlgrabar = "INSERT INTO usuarios(usuario, email ,password) value ('$nombre','$email','$pass')";
+       if(mysqli_query($conn,$sqlgrabar)){
+            echo "<script>window.location='PRUEBA.html'</script>";
+       }else{
+            echo "Error: ".$sql."<br>".mysqli_error($conn);
+        } 
+    }
